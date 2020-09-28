705159050796d0ca43f7460187731122d81cfa1b
sv-yarakaev <yarakaeff@gmail.com>
2020-09-28 22:13:01 +0300

    Remove spdlog and replace it to the std::cout

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "lib.h"
 
-#include <spdlog/spdlog.h>
+#include <iostream>
 
 int main()
 {
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("version {} was started", version());
+    //auto logger = spdlog::stdout_logger_mt("console");
+    std::cout <<"version " <<version() <<  " was started" << std::endl;
 
     return 0;
 }
